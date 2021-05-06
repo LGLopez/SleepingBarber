ce25dca950111337cd8c18b3489594b08e7b7f9d
Luis López <darkdestruktion@gmail.com>
2021-05-05 19:45:59 -0500

    Programa creo que listo!

--- SleepingBarber/Program.cs
+++ SleepingBarber/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Text.RegularExpressions;
 
 namespace SleepingBarber
 {
@@ -11,12 +12,32 @@ namespace SleepingBarber
             Random rand = new Random();
             int numberOfFreeWaitingRoomSeats = 3;                                                   // Cantidad de asientos libres
             int maxClients = 10;                                                                    // Cantidadad maxima de clientes
+            string cantidadClients;
+            
+            Console.WriteLine("Ingrese la cantidad de clientes que iran al barbero(limite = 100): ");
+            cantidadClients = Console.ReadLine();
+
+            Console.Clear();
+
+            if (Regex.IsMatch(cantidadClients, @"^\d+$"))
+            {
+                maxClients = Int16.Parse(cantidadClients);
+                if(maxClients > 100 || maxClients < 1)
+                {
+                    maxClients = 10;
+                    Console.WriteLine("Se introdujo una cantidad mayor o menor al limite, la cantidad de limites será 10");
+                }
+            }
+            else
+            {
+                Console.WriteLine("La entrada contenia valores diferentes a numeros, el valor de clientes será 10");
+            }
 
             Semaphore barberReady = new Semaphore(0, 1);                                            
             Semaphore accessWaitingRoomSeats = new Semaphore(1, 1);                                 // acceder al cuarto de espera
             Semaphore custReady = new Semaphore(0, 3);                                              // numero de clientes en el cuarto de espera
 
-            Console.WriteLine("Todo preparado");
+            Console.WriteLine("Presione escape o espere a que termine de atender los clientes para terminar.\n");
 
             void Barber()
             {
@@ -29,7 +50,7 @@ namespace SleepingBarber
 
                     barberReady.Release();
                     
-                    Console.WriteLine("El barbero corta el cabello de un cliente...");
+                    Console.WriteLine("El barbero esta cortando el cabello de un cliente...");
 
                     accessWaitingRoomSeats.Release();
                     
@@ -42,7 +63,7 @@ namespace SleepingBarber
 
                 int num = (int)number;
                 bool clientDone = false;
-                while (!clientDone)                                                                 // El cliente volvera hasta que se le pueda atender
+                while (!clientDone && !terminado)                                                                 // El cliente volvera hasta que se le pueda atender
                 {
                     Thread.Sleep(rand.Next(1, 9) * 1000);
 
@@ -67,11 +88,24 @@ namespace SleepingBarber
                         Console.WriteLine("Irse sin corte de cabello");
                     }
                 }
-
             }
 
+            void stopCode()
+            {
+                ConsoleKeyInfo cki;
+                cki = Console.ReadKey();
+                if (cki.Key == ConsoleKey.Escape)
+                {
+                    terminado = true;
+                    Environment.Exit(0);
+                }
+                    
+            }
+
+            Thread stopCodeThread = new Thread(stopCode);
             Thread barberThread = new Thread(Barber);
-            
+
+            stopCodeThread.Start();
             barberThread.Start();
 
             Thread[] Clients = new Thread[maxClients];
@@ -86,10 +120,13 @@ namespace SleepingBarber
             {
                 Clients[i].Join();
             }
-            
             terminado = true;
             barberThread.Join();
+            
             Console.WriteLine("El barbero vuelve a dormir!");
+            Console.WriteLine("Presione ESC para salir");
+
+            stopCodeThread.Join();
         }
     }
 }
